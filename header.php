6390db36d92c792d080a8bda0125aabecc26df04
Ale Raf <raffinalexis@gmail.com>
2024-04-29 10:49:19 +0200

    ajout balise script

--- header.php
+++ header.php
@@ -14,6 +14,7 @@ function setClass($title, $str)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
+    <script src="script.js" defer></script>
     <title>Yummy Nouilles - <?= $title ?></title>
 </head>
 
